2bb32ae7bdc8e9779bd8e629870d7d9419d97079
Scott-McG <scott.mcglashan@ocsbstudent.ca>
2017-09-18 13:01:54 -0400

    First Save-Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//this is a single line comment
+		/*
+		 * this is
+		 * a multiline
+		 * comment
+		 */
+		/**
+		 * this is my hello world program
+		 * author is your boy scotty 
+		 * yee
+		 */
+		System.out.println("Hello World!");
+		System.out.println("This is my first program");
+		System.out.println("Isn't this fun??");
+		System.out.println("He said \"Do this now!\"");
+		System.out.println();
+	}
+
+}
